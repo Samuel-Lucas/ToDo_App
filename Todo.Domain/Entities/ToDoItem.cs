cda3e4bebbccbee5e7fa8ed5cd12d563417a2056
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-04-24 20:39:39 -0300

    Entidade ToDoItem

---
+++ Todo.Domain/Entities/ToDoItem.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public class ToDoItem : Entity
+    {
+        public ToDoItem(string title, DateTime date, string user) : base(new Guid())
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUnDone()
+        {
+            Done = true;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
