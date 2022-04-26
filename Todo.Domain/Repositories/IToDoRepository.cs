ef7aed0778986110bc7b0a74647399a7f7154b55
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-04-25 23:42:26 -0300

    ToDo Repository

---
+++ Todo.Domain/Repositories/IToDoRepository.cs
@@ -0,0 +1,10 @@
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Repositories
+{
+    public interface IToDoRepository
+    {
+         void Create(ToDoItem todo);
+         void Update(ToDoItem todo);
+    }
+}
