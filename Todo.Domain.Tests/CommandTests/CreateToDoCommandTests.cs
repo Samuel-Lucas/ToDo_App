187995a3281f85851f874834218a075cb9146bc4
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-05-01 22:03:31 -0300

    Testes unitários no Command

---
+++ Todo.Domain.Tests/CommandTests/CreateToDoCommandTests.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateToDoCommandTests
+    {
+        private readonly CreateToDoCommand _invalidCommand = new CreateToDoCommand("",  DateTime.Now, "");
+        private readonly CreateToDoCommand _validCommand = new CreateToDoCommand("Título da tarefa",  DateTime.Now, "Samuel-Lucas");
+
+        public CreateToDoCommandTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            Assert.AreEqual(_invalidCommand.Valid, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido()
+        {
+            Assert.AreEqual(_validCommand.Valid, true);
+        }
+    }
+}
