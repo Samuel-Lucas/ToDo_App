2ed909b0dfc7c9e362749664f2e6999371ad4929
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-05-08 21:12:19 -0300

    Definindo atributos do modelo para o banco de dados

--- Todo.Domain.Infra/Context/ToDoContext.cs
+++ Todo.Domain.Infra/Context/ToDoContext.cs
@@ -9,6 +9,17 @@ namespace Todo.Domain.Infra.Context
         {
         }
 
-        public DbSet<ToDoItem> Todos { get; set; }  
+        public DbSet<ToDoItem> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItem>().ToTable("ToDo");
+            modelBuilder.Entity<ToDoItem>().Property(x => x.Id);
+            modelBuilder.Entity<ToDoItem>().Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<ToDoItem>().Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+            modelBuilder.Entity<ToDoItem>().Property(x => x.Done).HasColumnType("bit");
+            modelBuilder.Entity<ToDoItem>().Property(x => x.Date);
+            modelBuilder.Entity<ToDoItem>().HasIndex(b => b.User);
+        }
     }
 }
